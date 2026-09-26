4e55e8314d14fb0b8f6f5549fe758e3196fe527d
AlanaDubie <alanadubie922@gmail.com>
2026-09-25 22:04:24 -0700

    Base program from hw3

---
+++ TokenType.java
@@ -0,0 +1,24 @@
+//> Scanning token-type
+
+enum TokenType {
+    // Single-character tokens.
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, // CHALLENGE 1: Added COMMA
+    SEMICOLON, SLASH, STAR,
+    QUESTION, COLON, // CHALLENGE 2: Added QUESTION and COLON
+
+    // One or two character tokens.
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literals.
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords.
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
